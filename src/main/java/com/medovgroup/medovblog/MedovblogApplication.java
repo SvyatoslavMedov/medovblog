45078551d0d00d89fadce77f82c9e6055394c8bd
SvyatoslavMedov <irobot1204@gmail.com>
2023-03-16 21:01:25 +0600

    Initial commit

---
+++ src/main/java/com/medovgroup/medovblog/MedovblogApplication.java
@@ -0,0 +1,12 @@
+package com.medovgroup.medovblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedovblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedovblogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/medovgroup/medovblog/MedovblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medovgroup.medovblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedovblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
